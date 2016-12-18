eb72da0791c689e3ce7ef7048b2b5a3679a802c2
MicroDanjiao <xuxiang0226@163.com>
2016-12-18 21:00:35 +0800

    complete w2v

--- my_w2v.py
+++ my_w2v.py
@@ -1,55 +1,97 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import sys, math
+import sys, math, getopt
 
 import tensorflow as tf
 
+from text_dataset import TextDataset
+
 class MyW2V(object):
-    def __init__(self, vocab_size, batch_size=128, embedding_size=128, skip_window=1, num_sampled=64, 
-                num_step=1000, loss_freq=200):
+    def __init__(self, corpus_file, batch_size=128, embedding_size=128, skip_window=2, 
+                num_sampled=64, num_step=1000, loss_freq=200, num_per_win=2, min_cnt=2):
         self.batch_size = batch_size
         self.embedding_size = embedding_size
         self.skip_window = skip_window
-        self.vocab_size = vocab_size
         self.num_step = num_step
         self.num_sampled = num_sampled
         self.loss_freq = loss_freq
 
+        # corpus iteration
+        self.corpus = TextDataset(corpus_file, min_cnt=min_cnt, batch_size=batch_size, win_size=skip_window, num_per_win=num_per_win)
+        self.vocab_size = len(self.corpus.word2id)
+        print "init vocabulary size is", self.vocab_size
+
     def _init_param(self):
         self.embeddings = tf.Variable(tf.random_uniform([self.vocab_size, self.embedding_size], -1.0, 1.0))
-        self.nce_weights = tf.Variable(tf.truncated_normal([self.vocab_size, self.embedding_size], stddev=1.0/math.sqrt(embedding_size)))
+        self.nce_weights = tf.Variable(tf.truncated_normal([self.vocab_size, self.embedding_size], stddev=1.0/math.sqrt(self.embedding_size)))
         self.nce_biases = tf.Variable(tf.zeros([self.vocab_size]))
     
-    def fit(self.):
+    def fit(self):
         self._init_param()
-        init = tf.global_variable_initializer()
+        #init = tf.global_variables_initializer()
         train_inputs = tf.placeholder(tf.int32, shape=([self.batch_size]))
         train_labels = tf.placeholder(tf.int32, shape=([self.batch_size, 1]))
 
-        embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+        embed = tf.nn.embedding_lookup(self.embeddings, train_inputs)
 
-        norm = tf.sqrt(tf.reduce_sum(tf.square(self.embeddings)), 1, keep_dims=True)
+        norm = tf.sqrt(tf.reduce_sum(tf.square(self.embeddings), 1, keep_dims=True))
         normalized_embeddings = self.embeddings / norm
 
-        loss = tf.reduce_mean(tf.nn.nce_loss(nce_weight, nce_biases, embed, train_labels, self.num_sampled, self.vocab_size))
+        loss = tf.reduce_mean(tf.nn.nce_loss(self.nce_weights, self.nce_biases, embed, train_labels, self.num_sampled, self.vocab_size))
         optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
 
+        average_loss = 0
+        batch_iter = self.corpus.gen_batch_iter()
         with tf.Session() as session:
-            init.run()
+            session.run(tf.initialize_all_variables())
             print("init params OK ...")
             for step in xrange(self.num_step):
-                batch_inputs, batch_labels = generate_batch()
-                feed_dict = {train_inputs: train_inputs, train_labels: batch_labels}
+                batch_inputs, batch_labels = batch_iter.next()
+                feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
                 _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
                 average_loss += loss_val
 
                 if step % self.loss_freq == 0:
                     if step > 0:
                         average_loss /= 200
-                    print("Average loss at step:", step, ":", average_loss)
+                    print "Average loss at step:", step, ":", average_loss
                     average_loss = 0
-        self.final_embeddings = normalized_embeddings.eval()
+            self.final_embeddings = normalized_embeddings.eval()
+
+        self.corpus.close()
 
     def transform():
         pass
+
+def usage():
+    print "\t-h / --help"
+    print "\t--corpus: corpus_file (required))"
+    print "\t--vocab_size: the size of vocabulary (required)"        
+
+if __name__ == "__main__":
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "h", ["help", "corpus=", "vocab_size="])
+    except getopt.GetoptError:
+        print "%s usage:" % sys.argv[0]
+        usage()
+        sys.exit(1)
+
+    corpus = None
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print "%s usage:" % sys.argv[0]
+            usage()
+            sys.exit(1)
+        elif opt in ("--corpus"):
+            corpus = arg
+        
+    if not (corpus):
+        print "required arguments should be set!"
+        usage()
+        sys.exit(1)
+
+    w2v = MyW2V(corpus_file=corpus, batch_size=128, embedding_size=128, skip_window=2,
+                num_sampled=1, num_step=1000, loss_freq=200, num_per_win=2, min_cnt=2)
+
+    w2v.fit()
--- text_dataset.py
+++ text_dataset.py
@@ -23,7 +23,7 @@ class TextDataset(object):
     '''
         this class used to generate dataset for w2v
     '''
-    def __init__(self, filename, min_cnt=2, batch_size=128, win_size=1, num_per_win=win_size):
+    def __init__(self, filename, min_cnt=2, batch_size=128, win_size=1, num_per_win=1):
         if not os.path.isfile(filename):
             raise Exception("the file %s not found" % filename)
 
@@ -35,21 +35,20 @@ class TextDataset(object):
         assert num_per_win <= 2 * win_size
     
         self.rf = open(filename, "r")
-        _build_dataset()
+        self._build_dataset()
 
     def _build_dataset(self):
         counter = collections.Counter()
-        word2id = {"UNK", 0}
-        self.rf.seek(0, 0):
-        for line in rf.readline():
+        word2id = {"UNK": 0}
+        self.rf.seek(0, 0)
+        for line in self.rf:
             words = [strip_word(w.lower()) for w in line.strip().split()]
             counter.update(words)
 
-        print "vocabulary size: %d" %len(counter)
         unk_cnt = 0
         rm_word = []
-        for key, value in counter.iteritems
-            if value <= self.miin_cnt:
+        for key, value in counter.iteritems():
+            if value < self.min_cnt:
                 unk_cnt += value
                 rm_word.append(key)
             else:
@@ -59,30 +58,62 @@ class TextDataset(object):
             del counter[w]
         counter["UNK"] = unk_cnt
 
+        print "vocabulary size: %d" %len(counter)
         id2word = dict(zip(word2id.values(), word2id.keys()))
         self.word2id = word2id
         self.id2word = id2word
         self.wordcnt = counter
 
-    def generate_batch(self):
+    def gen_batch_iter(self):
         self.rf.seek(0,0)
         batch = np.ndarray(shape=(self.batch_size), dtype=np.int32)
-        target = np.ndarray(shape=(self.batch_size, 1), dtype=np.int32)
+        labels = np.ndarray(shape=(self.batch_size, 1), dtype=np.int32)
+        batch_idx = 0
         while True:
             line = self.rf.readline()
             if line == "":
                 self.rf.seek(0,0)
                 line = self.rf.readline()
+            #print line
             words = [strip_word(w.lower()) for w in line.strip().split()]
             if len(words) <= 1:
                 continue
             wids = [self.word2id.get(w, 0) for w in words]
+            #print wids
+            #[window target window]
             for i in range(len(wids)):
-                target = wids[i]
-                 context = 
-                
-                
+                center_id = wids[i]
+                ctx_ids = []
+                for j in range(i-self.win_size, i+self.win_size+1):
+                    if j != i and 0 <= j < len(wids):
+                        ctx_ids.append(wids[j])
 
+                # shuffle the context words
+                np.random.shuffle(ctx_ids)
+                for ctx_id in ctx_ids[:self.num_per_win]:
+                    batch[batch_idx] = center_id
+                    labels[batch_idx] = ctx_id
+                    batch_idx += 1
+            
+                    # one batch is filled
+                    if batch_idx == self.batch_size:
+                        yield batch, labels
+                        batch_idx = 0
+                        batch = np.ndarray(shape=(self.batch_size), dtype=np.int32)
+                        labels = np.ndarray(shape=(self.batch_size, 1), dtype=np.int32)
     
-    def close():
+    def close(self):
         self.rf.close()
+
+if __name__ == "__main__":
+    td = TextDataset("test", min_cnt=1, batch_size=5, win_size=2, num_per_win=2)
+    print td.word2id
+    print td.id2word
+    batch_iter = td.gen_batch_iter()
+    for x in range(15):
+        batch, labels = batch_iter.next()
+        print len(batch), batch
+        print len(labels), labels.ravel()
+        print "********"
+    td.close()
+
